cb719bf99a7ab610dcbb0080b2da5ad61750beb5
ArturDokhno <artur.dohno@icloud.com>
2022-04-08 19:16:21 +0500

    Initial Commit

---
+++ MVVM-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-1
+//
+//  Created by Артур Дохно on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
